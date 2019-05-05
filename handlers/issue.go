7795cc19f4b1c5b89406ba044c2914581107e58e
Marcos Riverola <marcosriverola@gmail.com>
2019-05-05 13:45:07 +0200

    Enabled CORS, api key and first get

--- handlers/issue.go
+++ handlers/issue.go
@@ -1,13 +1,46 @@
 package handlers
 
 import (
-	"fmt"
+	"asw-project/models"
+	"encoding/json"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
 
+func enableCors(w *http.ResponseWriter) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+}
+
+func authenticate(key string) bool {
+	users, _ := models.GetAllUsers()
+	exists := false
+	for _, s := range users {
+		if s.FirebaseID == key {
+			exists = true
+		}
+	}
+	return exists
+}
+
 func Index(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	fmt.Fprintf(w, vars["id"])
+
+	key := r.Header.Get("apiKey")
+	auth := authenticate(key)
+	if auth == true {
+		enableCors(&w)
+		vars := mux.Vars(r)
+
+		id, _ := strconv.Atoi(vars["id"])
+		issue, _ := models.FindIssueByID(uint(id))
+
+		w.Header().Set("Content-Type", "application/json")
+		j, _ := json.Marshal(issue)
+		w.WriteHeader(http.StatusOK)
+		w.Write(j)
+	} else {
+		w.Write([]byte("Cannot autenticate"))
+	}
+
 }
