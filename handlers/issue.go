ff8cf68df4f29f08a32306e014f4d54a81db7a9f
Marcos Riverola <marcosriverola@gmail.com>
2019-05-05 14:22:50 +0200

    hotfix cors

--- handlers/issue.go
+++ handlers/issue.go
@@ -11,6 +11,8 @@ import (
 
 func enableCors(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 }
 
 func authenticate(key string) bool {
